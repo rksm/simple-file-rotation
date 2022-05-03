03e994f05be37b3a23c955ebe27e14c04f8a69a3
Robert Krahn <robert.krahn@gmail.com>
2022-05-03 20:31:45 +0200

    readme update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! This is an implementation of simple [FileRotation] mechanism using only std.
+//! This is an implementation of simple [FileRotation](FileRotation) mechanism using only std.
 //! Given a file like `my.log`, it will copy that file to `my.1.log`, renaming a
 //! potentially pre-existing `my.1.log` to `my.2.log`. It accepts an optional
 //! number of max filesto keep. It will only rotate files when invoked, it will
